83a8b1e1b84c3f73dfe68005bb5ea08c7f29fd81
Your Name <belyashhh12@gmail.com>
2024-10-26 20:34:57 +0300

    #feat: lab 2, task 3

---
+++ lab_2/task_3.js
@@ -0,0 +1,35 @@
+// Исходная структура данных
+const events = [
+    { date: '2019-12', event: 'name1' },
+    { date: '2019-12', event: 'name2' },
+    { date: '2019-11', event: 'name3' },
+    { date: '2019-11', event: 'name4' },
+    { date: '2020-10', event: 'name5' },
+    { date: '2020-10', event: 'name6' },
+    { date: '2020-11', event: 'name5' },
+    { date: '2020-11', event: 'name6' },
+    { date: '2020-12', event: 'name7' },
+    { date: '2020-12', event: 'name8' },
+    { date: '2020-12', event: 'name9' },
+];
+
+// Результатная структура данных
+const result = {};
+
+// Перебираем все события
+for (const event of events) {
+    const year = parseInt(event.date.split('-')[0]); // Год
+    const month = parseInt(event.date.split('-')[1]); // Месяц
+
+    if (!result[year]) {
+        result[year] = {};
+    }
+
+    if (!result[year][month]) {
+        result[year][month] = [];
+    }
+
+    result[year][month].push(event.event);
+}
+
+console.log(result);
